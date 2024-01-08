d3dd72164addde8c1ad7dfd0ce250185ada69e71
xswitch <bivarmong@gmail.com>
2024-01-08 20:23:15 +0100

    Added the html and css

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operator, firstNum, secondNum) {
+    switch (operator) {
+        case '+':
+            return add(firstNum, secondNum);
+        case '-':
+            return subtract(firstNum, secondNum);
+        case '*':
+            return multiply(firstNum, secondNum);
+        case '/':
+            return divide(firstNum, secondNum);
+        default:
+            break;
+    }
+}
+
+let storedNumber = 0;
+let currentNumber = 0;
+let operator = '';
